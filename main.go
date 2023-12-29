f5ff09e89e886b6481d667ceacf8f59de864527a
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-29 17:25:55 -0500

    feat: add a handle to serve index.html

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 
 func main() {
 	const port = "8080"
+	const filepathRoot = "."
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 	corsMux := middlewareCors(mux)
 
 	server := &http.Server{
