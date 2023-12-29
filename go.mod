da1b17fd02e76dcec9524e82ae17dd8fe61ba083
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-29 17:20:35 -0500

    feat: start a server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tyler-lutz/chirpy
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	log.Printf("Server started at port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
