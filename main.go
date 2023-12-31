31fb06b0fadb8aa82a80413c0b4cdd7e47882152
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-31 17:35:24 -0500

    feat: functionality for deleting chirps

--- main.go
+++ main.go
@@ -63,6 +63,7 @@ func main() {
 	apiRouter.Post("/chirps", apiCfg.handlerChirpsCreate)
 	apiRouter.Get("/chirps", apiCfg.handlerChirpsRead)
 	apiRouter.Get("/chirps/{chirpID}", apiCfg.handlerChirpsReadOne)
+	apiRouter.Delete("/chirps/{chirpID}", apiCfg.handlerChirpsDelete)
 	router.Mount("/api", apiRouter)
 
 	adminRouter := chi.NewRouter()
